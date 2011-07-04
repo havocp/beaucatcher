644716ffa13a4cbc35eff4b2dac459f2145536ec
Havoc Pennington <hp@pobox.com>
2011-07-04 11:22:24 -0400

    add equals, hashCode, toString to MongoConfig

--- mongo/src/main/scala/org/beaucatcher/mongo/MongoConfig.scala
+++ mongo/src/main/scala/org/beaucatcher/mongo/MongoConfig.scala
@@ -8,6 +8,25 @@ trait MongoConfig {
     val databaseName : String
     val host : String
     val port : Int
+
+    override def equals(other : Any) : Boolean =
+        other match {
+            case that : MongoConfig =>
+                (that canEqual this) &&
+                    databaseName == that.databaseName &&
+                    host == that.host &&
+                    port == that.port
+            case _ => false
+        }
+
+    def canEqual(other : Any) : Boolean =
+        other.isInstanceOf[MongoConfig]
+
+    override def hashCode : Int =
+        41 * (41 * (41 + databaseName.hashCode) + host.hashCode) + port
+
+    override def toString =
+        "MongoConfig(%s,%s,%s)".format(databaseName, host, port)
 }
 
 class SimpleMongoConfig(override val databaseName : String,
