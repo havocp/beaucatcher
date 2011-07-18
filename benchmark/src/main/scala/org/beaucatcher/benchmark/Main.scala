dda2c7a6ad9c75c242d8aaadd12d14ca65ca5359
Havoc Pennington <hp@pobox.com>
2011-07-18 10:29:11 -0400

    Allow specifying a single benchmark to run

--- benchmark/src/main/scala/org/beaucatcher/benchmark/Main.scala
+++ benchmark/src/main/scala/org/beaucatcher/benchmark/Main.scala
@@ -2,9 +2,29 @@ package org.beaucatcher.benchmark
 
 object Main {
     def main(args : Array[String]) : Unit = {
+        val include =
+            if (args.length > 1) {
+                for (i <- 1 until args.length)
+                    yield args(i).toLowerCase.replace(" ", "")
+            } else {
+                Seq()
+            }
+
+        def includeBenchmark(b : MongoBenchmark[_]) = {
+            if (include.length == 0) {
+                true
+            } else {
+                include.contains(b.name.toLowerCase.replace(" ", ""))
+            }
+        }
+
         val benchmarks = Seq(new backends.RawJavaBenchmark,
             new backends.PlainCasbahBenchmark,
-            new backends.PlainHammersmithBenchmark)
+            new backends.PlainHammersmithBenchmark) filter includeBenchmark
+
+        if (include.length > 0)
+            printf("Enabling benchmarks: %s\n", benchmarks map { _.name } mkString ("", ",", ""))
+
         val benchmarker = new MongoBenchmarker
 
         benchmarker.run(benchmarks)
