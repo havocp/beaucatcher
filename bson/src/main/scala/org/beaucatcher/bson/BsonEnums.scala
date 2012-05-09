487c3903d279ef082ea1a9c9f2186fcd98ebcb02
Havoc Pennington <hp@pobox.com>
2012-05-09 19:16:40 -0400

    remove extra copy of BsonSubtype enum (it's sort of unclear to me why this compiled)

--- bson/src/main/scala/org/beaucatcher/bson/BsonEnums.scala
+++ bson/src/main/scala/org/beaucatcher/bson/BsonEnums.scala
@@ -25,30 +25,6 @@ object JsonFlavor extends Enumeration {
     val CLEAN, STRICT, JS, TEN_GEN = Value
 }
 
-/** A detailed type tag for binary data in Bson. */
-object BsonSubtype extends Enumeration {
-    type BsonSubtype = Value
-    val GENERAL, FUNC, BINARY, UUID, MD5, USER_DEFINED = Value
-
-    private val fromBytes =
-        Map(Bson.B_GENERAL -> GENERAL,
-            Bson.B_FUNC -> FUNC,
-            Bson.B_BINARY -> BINARY,
-            Bson.B_UUID -> UUID,
-            Bson.B_MD5 -> MD5,
-            Bson.B_USER_DEFINED -> USER_DEFINED)
-
-    private val toBytes = fromBytes map { _.swap }
-
-    def fromByte(b: Byte): Option[BsonSubtype] = {
-        fromBytes.get(b)
-    }
-
-    def toByte(v: Value): Byte = {
-        toBytes.getOrElse(v, throw new IllegalArgumentException("bad BsonSubtype value"))
-    }
-}
-
 /** Types that appear in the BSON wire protocol. See [[http://bsonspec.org/]] for more. */
 object BsonType extends Enumeration {
     type BsonType = Value
